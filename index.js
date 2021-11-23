d6464673159995d2e2b202f31fc96a490fdf1e0e
Adam Alfiansyah <adamalfiansyah@gmail.com>
2021-11-23 13:05:52 +0700

    cors

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ import express from "express";
 import Mongoose from 'mongoose';
 import ArticleController from "./controllers/ArticleController.js";
 
+const ORIGIN    = 'http://localhost:3200'
 const APP_PORT  = 8080
 const DB_URL    = 'mongodb+srv://wegodev:eh43gr34t27t32e3r@belajar.nafl5.mongodb.net/wegodev?retryWrites=true&w=majority'
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", ORIGIN);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 //setting endpoint yang dapat di akses user
 app.get('/api/articles', ArticleController.index);
